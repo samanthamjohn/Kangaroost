04ea1bf97b52ee3fd6a7a5a314bf0d041d6143a7
Samantha John <samantha.m.john@gmail.com>
2011-03-08 20:20:15 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kangaroost::Application.config.secret_token = '00d66e88e8960b0dbd16e99ab90ade53835133f12cf833ab338225df17ca4304c2579c9327b4eb4c9baa46e2de910a91c1b74ae79e2e765f4fff7beb14ca998d'
